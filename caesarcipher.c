757b455d3400310ab7671f00ec5355400559d405
Hema061 <113968433+Hema061@users.noreply.github.com>
2023-09-06 19:03:02 +0530

    Update caesarcipher.c

--- caesarcipher.c
+++ caesarcipher.c
@@ -1,34 +1,23 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-int main()
-{
-    int shift;
-    char plain[100];
-    char encrypted[100];
-    printf("Enter the key value: ");
-    scanf("%d", &shift);
-    printf("Enter the plain text: ");
-    scanf("%s", plain);
-    int len = strlen(plain);
-    for (int i = 0; i < len; i++)
-    {
-        if (isupper(plain[i]))
-        {
-            encrypted[i] = (plain[i] - 'A' + shift) % 26 + 'A';
-        }
-        else if (islower(plain[i]))
-        {
-            encrypted[i] = (plain[i] - 'a' + shift) % 26 + 'a';
-        }
-        else
-        {
-            encrypted[i] = plain[i];
-        }
-    }
-
-    encrypted[len] = '\0';
-
-    printf("The encrypted text is: %s\n", encrypted);
-    return 0;
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char
+	alp[53]="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+	char p[100];
+	int key;
+	printf("enter the plain text:");
+	scanf("%s",&p);
+	printf("enter the key value:");
+	scanf("%d",&key);
+	key=key%26;
+	printf("cypher text is:");
+	for(int i=0;i<strlen(p);i++){
+		for(int j=0;j<26;j++){
+			if(p[i]==alp[j]){
+				printf("%c",alp[j+key]);
+				break;
+			}
+		}
+	}
+	printf("\nplain text is:%s",p);
 }
